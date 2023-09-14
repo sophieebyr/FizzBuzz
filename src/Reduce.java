617c58ee23108e2a78b89e7f211b1f5d705284c1
sophierohrer <sophie.rohrer@mail.utoronto.ca>
2023-09-14 10:00:26 -0400

    Finished Lab 1 Reduce.java tasks

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Reduce {
+public class Reduce {
+    public static void main(String[] args) {
+        int steps = 0;
+        for (int i = 100; i > 0;) {
+            boolean even = i % 2 == 0;
+            if (even) {
+                i = i / 2;
+
+            } else {
+                i--;
+
+            }
+            steps++;
+
+        }
+        System.out.println(steps);
+    }
 }
