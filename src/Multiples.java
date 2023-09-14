90f010fad0c9185d35cd867615e0dda879f7de8d
sophierohrer <sophie.rohrer@mail.utoronto.ca>
2023-09-14 10:01:26 -0400

    Finished Lab 1 Multiples.java tasks

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Multiples {
+public class Multiples {
+    public  static void main(String[] args) {
+
+        int multiples = 0;
+        for (int i = 1; i < 1000; i++) {
+
+            // Find out which numbers divide i.
+            boolean multipleOf3 = i % 3 == 0;
+            boolean multipleOf5 = i % 5 == 0;
+
+            // Print our appropriate result.
+            if (multipleOf3 || multipleOf5) {
+                multiples++;
+
+
+
+        }
+    }
+        System.out.println(multiples);
+    }
+
+
 }
